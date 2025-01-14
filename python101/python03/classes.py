8e7526ba87153d306df98838c70e0ea486e70852
jules29 <djulestoure@yahoo.fr>
2025-01-14 05:49:28 +0100

    introduction classes

---
+++ python101/python03/classes.py
@@ -0,0 +1,20 @@
+
+
+class Car:
+    def __init__(self):
+        print("Car created")
+        self.engine_started = False
+        
+def engine_start(self, model):
+    self-engine_started = True
+    self.model = model
+    return f"The engine has started?  {self.engine_started} and its engine is {self.model} model"
+
+      
+ferarri = Car() #Instance of Carf class
+print(type(ferarri))
+ferarri.engine_started = True
+print(ferarri.engine_started)
+
+ferarri.engine_start()
+print(ferarri.engine_model)        
